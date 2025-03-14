bc42e43a8d9314b2a35380143cc77fc5e2220810
Avinash Jha <avijh200221024@gmail.com>
2025-03-14 18:44:19 +0530

    button improved

--- src/components/Buttonright.jsx
+++ src/components/Buttonright.jsx
@@ -4,10 +4,10 @@ import { FaAngleRight } from "react-icons/fa";
 const Buttonright = ({ info, bgColor, textColor, arrowBg, arrowColor}) => {
   return (
     <button className={`px-2 w-[253px] py-1 bg-[#283618]  rounded-full flex items-center justify-end gap-4`}>
-      <div className="flex items-center justify-center">
-        <p className="text-white text-lg text-center font-semibold">{info}</p>
+      <div className="flex items-center justify-center text-center mx-auto">
+        <p className="  text-white text-sm text-start font-semibold">{info}</p>
       </div>
-      <div className={`w-11 h-11 rounded-full ${arrowBg} flex items-center justify-center`}>
+      <div className={`w-11 h-11 rounded-full ${arrowBg} flex items-center justify-center -mr-[3px]`}>
         <FaAngleRight className={`${arrowColor} text-2xl`} />
       </div>
     </button>
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className=''>
+        <div className=''>
+
+        </div>
+
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
